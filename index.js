cdc471af9169443795e9d890894b5bc41c5690a7
Ajide Tosin Adebayo <47295073+adebayorajide@users.noreply.github.com>
2023-06-26 00:33:44 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+let display = document.getElementById("display");
+let buttons = document.querySelectorAll(".button");
+
+buttons.forEach((button)=>{
+    button.addEventListener("click", (e)=>{
+        console.log(e.target.innerText);
+        switch(e.target.innerText) {
+
+            case 'C':
+            display.innerText = "";
+            break;
+
+            case '+/-':
+                display.innerText = "-";
+                break;
+            case '=':
+                try {
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = 'Error'
+                }
+                break;
+               case '×':
+                if(display.innerText){
+                    display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    })
+})
