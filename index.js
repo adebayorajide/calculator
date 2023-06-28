e6af51b91e9892814e9070660f2c03eee3e819ec
Ajide Tosin Adebayo <47295073+adebayorajide@users.noreply.github.com>
2023-06-28 01:06:16 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
+const inputEl = document.querySelector(".input");
+const bodyEl = document.querySelector("body");
+inputEl.checked = JSON.parse(localStorage.getItem("mode"));
+
 let display = document.getElementById("display");
 let buttons = document.querySelectorAll(".button");
+let symbolEl = document.querySelectorAll(".operator")
+
+updateBody()
+
+function updateBody(){
+    if(inputEl.checked){
+        bodyEl.style.backgroundColor = "black"
+        buttons.forEach((button)=>{
+            button.style.color = "white"
+        })
+        display.style.color = "white"
+
+    } else {
+        bodyEl.style.backgroundColor = "white"
+        buttons.forEach((button)=>{
+            button.style.color = "black"
+        })
+        symbolEl.forEach((sym)=>{
+            sym.style.color = "white"
+        })
+        display.style.color = "black"
+
+    }
+}
+
+inputEl.addEventListener("input", ()=>{
+    updateBody();
+    updatelocalstorage();
+})
+
+function updatelocalstorage(){
+    localStorage.setItem("mode", JSON.stringify(inputEl.checked))
+}
 
 buttons.forEach((button)=>{
     button.addEventListener("click", (e)=>{
